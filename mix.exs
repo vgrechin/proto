66235ce548f73a46c7a65c35dec2c8275cf0913c
Slava Grechin <vgrechin@yandex.ru>
2020-04-04 21:40:55 +0300

    Exlixir CI max version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Proto.MixProject do
     [
       app: :proto,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
