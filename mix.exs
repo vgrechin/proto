10d1c09fca4af6fc4ec2e814dbd71c86c3f30465
Slava Grechin <slava.grechin@gmail.com>
2020-04-04 21:18:07 +0300

    Dependencies update

--- mix.exs
+++ mix.exs
@@ -21,8 +21,9 @@ defmodule Proto.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:cowboy, github: "extend/cowboy"},
+      {:mimetypes, github: "spawngrid/mimetypes"},
+      {:protobuf, github: "tony612/protobuf-elixir"}
     ]
   end
 end
