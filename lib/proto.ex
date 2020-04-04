a814eb05460567bb96a8075da5ffc9c2fae97221
Slava Grechin <slava.grechin@gmail.com>
2020-04-04 21:17:07 +0300

    Default mix project

---
+++ lib/proto.ex
@@ -0,0 +1,18 @@
+defmodule Proto do
+  @moduledoc """
+  Documentation for `Proto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Proto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
